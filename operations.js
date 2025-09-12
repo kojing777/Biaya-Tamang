54b522a7145b5d369cbe7d1e165b5aa05be6c61b
kojing777 <kojingmoktan92@gmail.com>
2025-09-12 23:37:34 +0545

    Operations

---
+++ operations.js
@@ -0,0 +1,47 @@
+// let a = 10;
+// let b= 20;
+
+// console.log (a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+// let x = 10;
+// x += 5; 
+// x *= 2; 
+// console.log(x); 
+
+let num1= 7;
+let num2= 4;
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1%num2);
+console.log(num1**num2);
+
+let score = 10;
+score += 5;
+score *= 2;
+console.log(score);
+
+console.log(num1>num2);
+console.log(num1<num2);
+console.log(num1>=num2);
+console.log(num1<=num2);
+console.log(num1==num2);
+console.log(num1!=num2);
+
+
+//logical operators
+let isRaining = true;
+let haveUmbrella = false;
+console.log(isRaining && haveUmbrella);
+console.log(isRaining || haveUmbrella);
+console.log(!isRaining);
+
+//exponential operator
+(num1 + num2) * 3 - 5
+console.log((num1 + num2) * 3 - 5);
