5f8aa015363f799af5ddcd8ed01009ea5988d391
kojing777 <kojingmoktan92@gmail.com>
2025-09-12 17:56:28 +0545

    First Commit

---
+++ start.js
@@ -0,0 +1,15 @@
+// let age = 21;
+// const country = "Nepal";
+// var mood = "Happy";
+
+// console.log("I am", age, "years old from", country, "and I feel", mood);
+
+// let city= "kathmandu";
+// console.log("i live in",city);
+
+// city="kojingar";
+// console.log("i live in",city);
+
+let age;
+age=20;
+console.log("i am",age,"year old");
