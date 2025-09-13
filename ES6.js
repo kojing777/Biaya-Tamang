49372c4241c04a4734889614574e24b1247b3e50
kojing777 <kojingmoktan92@gmail.com>
2025-09-13 23:23:13 +0545

    ES62

--- ES6.js
+++ ES6.js
@@ -15,3 +15,39 @@ console.log(s + " points");
 let s2 = n.toString();
 console.log(s2);
 
+//Boolean Conversion
+console.log(Boolean(0));   
+console.log(Boolean(1));   
+console.log(Boolean(""));  
+console.log(Boolean("Hello"));
+console.log(Boolean(null)); 
+console.log(Boolean(undefined)); 
+
+
+////String Operators
+//Concatenation
+let firstName="Bijaya";
+let lastName="Tamang";
+console.log(firstName + " " + lastName);
+
+//String Methods
+// let str="Hello, World!";
+// console.log(str.length); 
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.indexOf("World")); 
+// console.log(str.slice(0, 5)); 
+// console.log(str.replace("World", "JavaScript"));
+
+let kojing="Hello, Bijaya!";
+console.log(kojing.toUpperCase());
+console.log(kojing.toLowerCase());
+console.log(kojing.trim());
+console.log(kojing.includes("Bijaya"));
+console.log(kojing.startsWith("H"));
+console.log(kojing.endsWith("!"));
+console.log(kojing.split(","));
+console.log(kojing.charAt(7));
+console.log(kojing.indexOf("B"));
+console.log(kojing.lastIndexOf("a"));
+console.log(kojing.replace("Bijaya", "Sanu"));
