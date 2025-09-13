f508caf09eb010c30b86a9adbd251bd24933af41
kojing777 <kojingmoktan92@gmail.com>
2025-09-13 22:44:41 +0545

    ES61

---
+++ ES6.js
@@ -0,0 +1,17 @@
+// let str = "123"; 
+// let num = Number(str); 
+// console.log(num + 1);
+
+
+// let num="12345";
+// let str=String(num);
+// console.log(str + 1);
+
+let n = 100; 
+let s = String(n);
+console.log(s + " points");
+
+
+let s2 = n.toString();
+console.log(s2);
+
