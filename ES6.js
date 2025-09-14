7c95a490f21f212435f584e1af17e00421fe70ad
kojing777 <kojingmoktan92@gmail.com>
2025-09-14 22:06:40 +0545

    number and string conversions

--- ES6.js
+++ ES6.js
@@ -7,28 +7,28 @@
 // let str=String(num);
 // console.log(str + 1);
 
-let n = 100; 
-let s = String(n);
-console.log(s + " points");
+// let n = 100; 
+// let s = String(n);
+// console.log(s + " points");
 
 
-let s2 = n.toString();
-console.log(s2);
+// let s2 = n.toString();
+// console.log(s2);
 
 //Boolean Conversion
-console.log(Boolean(0));   
-console.log(Boolean(1));   
-console.log(Boolean(""));  
-console.log(Boolean("Hello"));
-console.log(Boolean(null)); 
-console.log(Boolean(undefined)); 
+// console.log(Boolean(0));   
+// console.log(Boolean(1));   
+// console.log(Boolean(""));  
+// console.log(Boolean("Hello"));
+// console.log(Boolean(null)); 
+// console.log(Boolean(undefined)); 
 
 
 ////String Operators
 //Concatenation
-let firstName="Bijaya";
-let lastName="Tamang";
-console.log(firstName + " " + lastName);
+// let firstName="Bijaya";
+// let lastName="Tamang";
+// console.log(firstName + " " + lastName);
 
 //String Methods
 // let str="Hello, World!";
@@ -39,15 +39,24 @@ console.log(firstName + " " + lastName);
 // console.log(str.slice(0, 5)); 
 // console.log(str.replace("World", "JavaScript"));
 
-let kojing="Hello, Bijaya!";
-console.log(kojing.toUpperCase());
-console.log(kojing.toLowerCase());
-console.log(kojing.trim());
-console.log(kojing.includes("Bijaya"));
-console.log(kojing.startsWith("H"));
-console.log(kojing.endsWith("!"));
-console.log(kojing.split(","));
-console.log(kojing.charAt(7));
-console.log(kojing.indexOf("B"));
-console.log(kojing.lastIndexOf("a"));
-console.log(kojing.replace("Bijaya", "Sanu"));
+// let kojing="Hello, Bijaya!";
+// console.log(kojing.toUpperCase());
+// console.log(kojing.toLowerCase());
+// console.log(kojing.trim());
+// console.log(kojing.includes("Bijaya"));
+// console.log(kojing.startsWith("H"));
+// console.log(kojing.endsWith("!"));
+// console.log(kojing.split(","));
+// console.log(kojing.charAt(7));
+// console.log(kojing.indexOf("B"));
+// console.log(kojing.lastIndexOf("a"));
+// console.log(kojing.replace("Bijaya", "Sanu"));
+
+//exercise
+let str="2025";
+let num=Number(str);
+console.log(num + 10);
+
+let n=99;
+let s=String(n);
+console.log(s + "points");
--- Template.js
+++ Template.js
@@ -1,9 +1,31 @@
 //TEMPLATE STRING
-let name = "Bijaya";
-let age = 21;
-console.log(`My name is ${name} and I am ${age} years old.`);
+// let name = "Bijaya";
+// let age = 21;
+// console.log(`My name is ${name} and I am ${age} years old.`);
 
+// let msg = `This is line 1
+// This is line 2`;
+// console.log(msg);
 
-let msg = `This is line 1
-This is line 2`;
-console.log(msg);
+// console.log(`Next year I will be ${age + 1}`);
+
+let str="2025";
+let num=Number(str);
+console.log(num + 10);
+
+let n=99;
+let s=String(n);
+console.log(s + " " + "points");
+
+let text="Hello, Bijaya!";
+console.log(text.trim());
+console.log(Boolean(" "));
+console.log(Boolean("js"));
+
+let firstName="Hello";
+let lastName="World";
+console.log(firstName + ", " + lastName + "!");
+
+let num1=7;
+let num2=8;
+console.log(`my favorite number is ${num1} and next year it will be ${num2}.`)
