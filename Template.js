e6d8209ec8d07d997d5444aa2f7b30df5990107f
kojing777 <kojingmoktan92@gmail.com>
2025-09-14 12:27:33 +0545

    template string

--- Template.js
+++ Template.js
@@ -1,5 +1,9 @@
+//TEMPLATE STRING
 let name = "Bijaya";
 let age = 21;
-
 console.log(`My name is ${name} and I am ${age} years old.`);
 
+
+let msg = `This is line 1
+This is line 2`;
+console.log(msg);
