2d815239290238bbc5c5ec9b57d59ffd508b900c
kojing777 <kojingmoktan92@gmail.com>
2025-09-15 17:32:15 +0545

    switch case

---
+++ Switch.js
@@ -0,0 +1,75 @@
+let day = 4;
+switch (day) {
+    case 1:
+        console.log("sunday");
+        break;
+    case 2:
+        console.log("monday");
+        break;
+    case 3:
+        console.log("tuesday");
+        break;
+    case 4:
+        console.log("wednesday");
+        break;
+    default:
+        console.log("invalid day");
+}
+
+
+let month = 4;
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("august");
+        break;
+    case 9:
+        console.log("september");
+        break;
+    case 10:
+        console.log("october");
+        break;
+    case 11:
+        console.log("november");
+        break;
+    case 12:
+        console.log("december");
+        break;
+    default:
+        console.log("invalid date")
+}
+
+let color = "red";
+switch (color) {
+    case "red":
+        console.log("stop");
+    case "yellow":
+        console.log("ready");
+    case "green":
+        console.log("go");
+    default:
+        console.log("invalid color")
+}
+
+let a = 5;
+let b = 10;
