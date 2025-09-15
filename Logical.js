132533bdab5a05c7fc06763a51076442a403cb58
kojing777 <kojingmoktan92@gmail.com>
2025-09-15 15:14:58 +0545

    logical conditions and

--- Logical.js
+++ Logical.js
@@ -1,4 +1,30 @@
-let age = 19;
-if (age > 18 && age < 20){
-    console.log("Young Adult");
+// let age = 25;
+
+// if (age > 18 && age < 30) {
+//   console.log("Young adult");
+// }
+
+// if (age < 18 || age > 60) {
+//   console.log("Special age group");
+// }
+
+// let isStudent = false;
+// if (!isStudent) {
+//   console.log("Not a student");
+// }
+
+
+let n = 15;
+if (n > 10 && n < 20){
+    console.log(" n is lies between 10 and 20");
+}
+
+let age = 70;
+if (age <= 18 || age >= 65) {
+    console.log("either below 18 OR above 65.");
+}
+
+let isLoggedIn = false;
+if (!isLoggedIn) {
+    console.log("User is logged out");
 }
