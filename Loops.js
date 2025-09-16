e1e603f3624be1243a520b8f0439a1702cc252a3
kojing777 <kojingmoktan92@gmail.com>
2025-09-16 22:21:08 +0545

    while loop examples

--- Loops.js
+++ Loops.js
@@ -1,11 +1,19 @@
 //for loops
-for (let i = 1; i <= 5; i++) {
-    console.log(i);
-}
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
 
 // while loops
-let i = 1;
-while (i <= 5) {
-    console.log(i);
-    i++;
-}
+// let i = 1;
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// do while loops
+let j = 6;
+do {
+    console.log(j);
+    j++;
+} while (j <= 10);
+
