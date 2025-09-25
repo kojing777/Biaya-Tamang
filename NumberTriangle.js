557ae8efce85df89abef66d9a12c7d564a64294e
kojing777 <kojingmoktan92@gmail.com>
2025-09-25 23:20:29 +0545

    Add NumberTriangle.js to demonstrate nested loops for generating number patterns

---
+++ NumberTriangle.js
@@ -0,0 +1,15 @@
+for (let i=1;i<=5;i++){
+    let row='';
+    for (let j=1;j<=i;j++){
+        row+=j+' ';
+    }
+    console.log(row);
+}
+
+for (let j=1;j<=5;j++){
+    let column='';
+    for (let i=1;i<=j;i++){
+        column+=j+' ';
+    }
+    console.log(column);
+}
