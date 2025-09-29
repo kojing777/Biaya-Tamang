43d9dac5abfa441a43f85cf95a4223d0ea9943fd
kojing777 <kojingmoktan92@gmail.com>
2025-09-29 20:18:52 +0545

    Add greetUser function

--- Function.js
+++ Function.js
@@ -2,3 +2,11 @@ function greet() {
     console.log("Hello, Bijaya!");
 }
 greet();  // calling the function
+
+
+//Function with Parameters
+function greetUser(name) {
+    console.log("Hello, " + name + "!");
+}
+greetUser("Bijaya");
+greetUser("Ram");
