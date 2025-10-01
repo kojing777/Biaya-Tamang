a00a7e2c79e9b117341f3926a059ce9a75d03bd4
kojing777 <kojingmoktan92@gmail.com>
2025-10-01 22:02:06 +0545

    Function.js clarity

--- Function.js
+++ Function.js
@@ -1,19 +1,19 @@
-function greet() {
-    console.log("Hello, Bijaya!");
-}
-greet();  // calling the function
+// function greet() {
+//     console.log("Hello, Bijaya!");
+// }
+// greet();
 
 
 //Function with Parameters
-function greetUser(name) {
-    console.log("Hello, " + name + "!");
-}
-greetUser("Bijaya");
-greetUser("Ram");
+// function greetUser(name) {
+//     console.log("Hello, " + name + "!");
+// }
+// greetUser("Bijaya");
+// greetUser("Ram");
 
 //Function with Return Value
-function add(a, b) {
-    return a + b;
-}
-let sum = add(5, 7);
-console.log("Sum is:", sum);
+// function add(a, b) {
+//     return a + b;
+// }
+// let sum = add(5, 7);
+// console.log("Sum is:", sum);
