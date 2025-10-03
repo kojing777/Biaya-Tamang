e1589254fe35d792cccce821ed2b7afb6f351a7c
kojing777 <kojingmoktan92@gmail.com>
2025-10-03 21:47:32 +0545

    ATM Balance Checker

--- Function.js
+++ Function.js
@@ -182,16 +182,26 @@
 // console.log(isPalindrome('hello'))
 
 //fibonacci
-function fibonacci(n){
-    let a = 0,b=1;
-    console.log(a);
-    console.log(b);
+// function fibonacci(n){
+//     let a = 0,b=1;
+//     console.log(a);
+//     console.log(b);
 
-    for (let i =3;i<=n;i++){
-        let next = a+b;
-        console.log(next);
-        a=b;
-        b=next;
+//     for (let i =3;i<=n;i++){
+//         let next = a+b;
+//         console.log(next);
+//         a=b;
+//         b=next;
+//     }
+// }
+// fibonacci(7);
+
+//ATM Balance Checker
+function chckBalance (balance,withdraw){
+    if(withdraw > balance){
+        return 'insufficient balance !';
+    }else{
+        return `withdrawal succeccfull. remainng balance:${balance-withdraw}`;
+
     }
-}
-fibonacci(7);
+}console.log(chckBalance(1000,5555))
